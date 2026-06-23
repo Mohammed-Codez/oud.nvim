d9903123d8af6583a183d586cc1f194c2bc6df0a
Mohammed-Codez <amakactive@gmail.com>
2026-06-23 19:03:28 +0400

    everything is finally working properly. just need to find out how to do it cleanly

--- lua/oud/highlights.lua
+++ lua/oud/highlights.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.setup(palette)
 	local set = vim.api.nvim_set_hl
 
-	set(0, "Normal", { fg = "#ff0000", bg = palette.bg_xl })
+	set(0, "Normal", { fg = palette.fg_xl, bg = palette.bg_xl })
 end
 
 return M
